e09cc1134ed9c526ac36c590f8495377e9b59e66
Zheeeng <air.island.e@gmail.com>
2016-10-06 23:57:37 +0800

    Add draw.js

---
+++ scripts/Draw.js
@@ -0,0 +1,35 @@
+var Draw = function (canvasEl, width, height, bgColor, lineWidth) {
+  Canvas.apply(this, arguments)
+  this.isPainting = false
+}
+
+extend(Draw, Canvas)
+
+Draw.prototype.init = function () {
+  Draw.uber.init.apply(this, arguments)
+
+  var canvas = this.canvas
+  var ctx = this.ctx
+  var _this = this
+  addEvent(canvas, 'mousedown', function (e) {
+    _this.isPainting = true
+    _this.lastX = _this.x
+    _this.lastY = _this.y
+  })
+  addEvent(canvas, 'mouseup', function (e) {
+    _this.isPainting = false
+  })
+  addEvent(canvas, 'mouseout', function (e) {
+    _this.isPainting = false
+  })
+  addEvent(canvas, 'mousemove', function (e) {
+    if (_this.isPainting === true) {
+      ctx.beginPath()
+      ctx.moveTo(_this.lastX, _this.lastY)
+      ctx.lineTo(_this.x, _this.y)
+      ctx.stroke()
+      _this.lastX = _this.x
+      _this.lastY = _this.y
+    }
+  })
+}
