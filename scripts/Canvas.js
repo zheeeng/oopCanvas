4264a5756f9e73a3674784f10f91a596108ed005
Zheeeng <air.island.e@gmail.com>
2016-10-07 19:48:05 +0800

    Reorganize

--- scripts/Canvas.js
+++ scripts/Canvas.js
@@ -1,34 +1,143 @@
-var Canvas = function (canvasEl, width, height) {
-  this.canvas = canvasEl !== undefined ? canvasEl : document.getElementsByTagName('canvas')[0]
-  this.width = width !== undefined ? width : 500
-  this.height = height !== undefined ? height : 300
+var Canvas = function (canvasEl, layout) {
+  this.canvas = isCanvasElement(canvasEl) ? canvasEl : document.getElementsByTagName('canvas')[0]
+  this.layout = Canvas.formatLayoutData(layout)
 
   this.ctx = this.canvas.getContext('2d')
 
   this._e = null
 }
 
-Object.defineProperties(Canvas.prototype, {
-  x: {
-    get: function () {
-      return this._e.pageX - this.canvas.offsetLeft
-    }
-  },
-  y: {
-    get: function () {
-      return this._e.pageY - this.canvas.offsetTop
+Canvas.formatLayoutData = function (layout) {
+  var width = layout.width
+  var height = layout.height
+  var row = layout.row
+  var column = layout.column
+  var zones = layout.zones
+
+  // format width && height
+  if (isNaN(width) || isNaN(height)) {
+    throw TypeError('The width and the height of layout are required and they are numbers')
+  } else {
+    layout.width = +width
+    layout.height = +height
+  }
+
+  // format row && column
+  if (isNaN(row) || isNaN(column)) {
+    throw TypeError('The row -- ' + row + ', and the column -- ' + column + ', of canvas grid are required and they are must be numbers.')
+  } else {
+    if (row < 1) throw Error('Invalid canvas grid row number -- ' + row + ', it must be not less than 1.')
+    if (column < 1) throw Error('Invalid canvas grid column number -- ' + column + ', it must be not less than 1.')
+    layout.row = Math.floor(row)
+    layout.column = Math.floor(column)
+  }
+
+  // The row && column number must be integer and in range [1, grid max row/column]
+  function formatRow (row) {
+    if (isNaN(row)) {
+      throw TypeError('The canvas grid row number: ' + row + ' must be a number')
+    } else if (row < 1 || row > layout.row){
+      throw TypeError('The canvas grid row number: ' + row + ' is not in range [1, ' + layout.row + ']')
+    } else {
+      return Math.floor(row)
     }
   }
-})
+
+  function formatColumn (column) {
+    if (isNaN(column)) {
+      throw TypeError('The canvas grid column number: ' + column + ' must be a number')
+    } else if (column < 1 || column > layout.column){
+      throw TypeError('The canvas grid column number: ' + column + ' is not in range [1, ' + layout.column + ']')
+    } else {
+      return Math.floor(column)
+    }
+  }
+
+  // format zones data
+  zones.forEach(function (zone) {
+    zone.rows = zone.rows.map(function (row) {
+      return formatRow(row)
+    }).sort(function (a, b) {
+      return a - b
+    })
+    zone.columns = zone.columns.map(function (column) {
+      return formatColumn(column)
+    }).sort(function (a, b) {
+      return a - b
+    })
+  })
+
+  return layout
+}
 
 Canvas.prototype.init = function () {
+  var canvas = this.canvas
+  var layout = this.layout
+
+  // set canvas attributes
+  canvas.width = layout.width
+  canvas.height = layout.height
+
+  // record the cursor position in canvas
   var _this = this
-  var canvas = _this.canvas
-  canvas.width = _this.width
-  canvas.height = _this.height
   addEvent(canvas, 'mousemove', function (e) {
     _this._e = e
   })
+
+  Object.defineProperties(Canvas.prototype, {
+    x: {
+      get: function () {
+        return this._e.pageX - this.canvas.offsetLeft
+      }
+    },
+    y: {
+      get: function () {
+        return this._e.pageY - this.canvas.offsetTop
+      }
+    }
+  })
+
+  // store zones from layout option
+  this._zones = Canvas.getZones(this.layout)
+
+}
+
+/*
+ * Get zones array, require layout { Number width, Number height, Array zones [ Number Array[2] columns, Number Array[2] rows, Object props ] }
+ * @para layout
+ * @return zonesArray
+ */
+Canvas.getZones = function (layout) {
+  var unitWidth
+  var unitHeight
+  var zones = layout.zones
+  var zonesArray = []
+
+  // define the validator for check zones conflict
+  function isZoneValidate (zone) {
+    return zonesArray.every(function (v) {
+      return (v.fromX >= zone.toX || v.fromY >= zone.toY) || (v.toX <= zone.fromX || v.toY <= zone.fromY)
+    })
+  }
+
+  // get unit width && height
+  unitWidth = Math.round(layout.width / layout.column, 2)
+  unitHeight = Math.round(layout.height / layout.row, 2)
+
+  // register zones
+  zones.forEach(function (zone) {
+    var _zone = {
+      fromX : (zone.columns[0] - 1) * unitWidth,
+      toX   : zone.columns[1] * unitWidth,
+      fromY : (zone.rows[0] - 1) * unitHeight,
+      toY   : zone.rows[1] * unitHeight,
+      props  : zone.props
+    }
+    if (!isZoneValidate(_zone)) throw Error('custom zone: ' + JSON.stringify(zone) + ' conflicts with other zones')
+    zonesArray.push(_zone)
+  })
+
+  return zonesArray
 }
 
 Canvas.prototype.clearCanvas = function () {
--- scripts/Draw.js
+++ scripts/Draw.js
@@ -1,6 +1,5 @@
-var Draw = function (canvasEl, width, height, layouts) {
-  Canvas.call(this, canvasEl, width, height)
-  this.layouts = Draw.formatLayoutData(layouts)
+var Draw = function (canvasEl, layouts) {
+  Canvas.call(this, canvasEl, layouts)
 
   this._isPainting = false
 }
@@ -9,15 +8,32 @@ extend(Draw, Canvas)
 
 Draw.prototype.init = function () {
   Draw.uber.init.apply(this, arguments)
+  this.drawZones()
+  this.active()
+}
 
+Draw.prototype.drawZones = function () {
+  if (this._zones === undefined) throw Error('no zones to draw')
+
+  var zones = this._zones
+  var ctx = this.ctx
+  zones.forEach(function (zone) {
+    if (zone.type === 'painting') {
+      console.log(zone)
+      ctx.save()
+      ctx.rect(zone.fromX, zone.fromY, zone.toX - zone.fromX, zone.toY - zone.fromY)
+      ctx.stroke()
+      ctx.restore()
+    } else if (zone.type === 'tool') {
+      console.log('draw tool zone')
+    }
+  })
+}
+
+Draw.prototype.active = function () {
   var _this = this
   var canvas = _this.canvas
   var ctx = _this.ctx
-
-  this.layout()
-
-  this.drawZones()
-
   addEvent(canvas, 'mousedown', function (e) {
     _this._isPainting = true
     _this.lastX = _this.x
@@ -37,108 +53,6 @@ Draw.prototype.init = function () {
       ctx.stroke()
       _this.lastX = _this.x
       _this.lastY = _this.y
-    }
-  })
-}
-
-Draw.formatLayoutData = function (layouts) {
-  var zones = layouts.zones
-
-  // get unit width && height
-  if (isNaN(layouts.row) || isNaN(layouts.column)) {
-    throw TypeError('row && column is required and they are numbers')
-  } else {
-    if (layouts.row < 1) throw Error('invalid row number, it must be not less than 1')
-    if (layouts.column < 1) throw Error('invalid column number, it must be not less than 1')
-    layouts.row = Math.floor(layouts.row)
-    layouts.column = Math.floor(layouts.column)
-  }
-
-  function formatRow (row) {
-    if (isNaN(row)) {
-      throw TypeError('row number:' + row + 'must be a number')
-    } else if (row < 1 || row > layouts.row){
-      throw TypeError('row number:' + row + 'is not in range [1, ' + layouts.row + ']')
-    } else {
-      return Math.floor(row)
-    }
-  }
-
-  function formatColumn (column) {
-    if (isNaN(column)) {
-      throw TypeError('column number:' + column + 'must be a number')
-    } else if (column < 1 || column > layouts.column){
-      throw TypeError('column number:' + column + 'is not in range [1, ' + layouts.column + ']')
-    } else {
-      return Math.floor(column)
-    }
-  }
-
-  // format zones data
-  zones.forEach(function (zone) {
-    zone.rows = zone.rows.map(function (row) {
-      return formatRow(row)
-    }).sort(function (a, b) {
-      return a - b
-    })
-    zone.columns = zone.columns.map(function (column) {
-      return formatColumn(column)
-    }).sort(function (a, b) {
-      return a - b
-    })
-  })
-
-  return layouts
-}
-
-Draw.prototype.layout = function () {
-  var unitWidth
-  var unitHeight
-  var layouts = this.layouts
-  var zones = layouts.zones
-  var zonesArray = []
-
-  // define the validator for check zones conflict
-  function checkZoneIsValidate (zone) {
-    return zonesArray.every(function (v) {
-      return (v.fromX >= zone.toX || v.fromY >= zone.toY) || (v.toX <= zone.fromX || v.toY <= zone.fromY)
-    })
-  }
-
-  this.clearCanvas()
-
-  // get unit width && height
-  unitWidth = Math.round(this.width / layouts.column, 2)
-  unitHeight = Math.round(this.height / layouts.row, 2)
-
-  // register zones
-  zones.forEach(function (zone) {
-    var _zone = {
-      fromX : (zone.columns[0] - 1) * unitWidth,
-      toX   : zone.columns[1] * unitWidth,
-      fromY : (zone.rows[0] - 1) * unitHeight,
-      toY   : zone.rows[1] * unitHeight,
-      type  : zone.type
-    }
-    if (!checkZoneIsValidate(_zone)) throw Error('custom zone:' + zone + 'conflicts with other zones')
-    zonesArray.push(_zone)
-  })
-
-  this.zones = zonesArray
-}
-
-Draw.prototype.drawZones = function () {
-  if (this.zones === undefined) throw Error('no zones to draw')
-
-  var zones = this.zones
-  var ctx = this.ctx
-  zones.forEach(function (zone) {
-    if (zone.type === 'painting') {
-      console.log(zone)
-      ctx.save()
-      ctx.rect(zone.fromX, zone.fromY, zone.toX - zone.fromX, zone.toY - zone.fromY)
-      ctx.stroke()
-      ctx.restore()
     }
   })
 }
